5f6fa22ac4f79fb5accaa1c47e092c99094fe81f
Tiger <tigerma@berkeley.edu>
2023-05-29 15:44:24 -0700

    uibuild

--- index.js
+++ index.js
@@ -36,27 +36,39 @@ app.get('/api/persons', (request, response) => {
 //   `)
 // })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id)
-    .then((person) => {
-      response.json(note)
-    })
-    .catch((error) => {
+app.get('/api/persons/:id', async (request, response, next) => {
+  // Person.findById(request.params.id)
+  //   .then((person) => {
+  //     if (person) {
+  //       response.json(note)
+  //     } else {
+  //       response.status(404).end()
+  //     }
+  //   })
+  //   .catch((error) => next(error))
+  try {
+    const person = await Person.findById(request.params.id)
+    if (person) {
+      response.json(person)
+    } else {
       response.status(404).end()
-    })
-  // const id = Number(request.params.id)
-
-  // const person = persons.find((person) => person.id === id)
-  // if (person) {
-  //   response.json(person)
-  // } else {
-  //   response.status(404).end()
-  // }
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.delete('/api/persons/:id', async (request, response) => {
-  await Person.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+app.delete('/api/persons/:id', async (request, response, next) => {
+  try {
+    const result = await Person.findByIdAndRemove(request.params.id)
+    console.log(result)
+    if (!result) {
+      console.log('already deleted')
+    }
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
   // const id = Number(request.params.id)
   // persons = persons.filter((person) => person.id !== id)
   // response.status(204).end()
@@ -66,13 +78,13 @@ app.delete('/api/persons/:id', async (request, response) => {
 //   return Math.floor(Math.random() * 9999999)
 // }
 
-app.post('/api/persons', async (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
   const body = request.body
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'missing name and/or number',
-    })
-  }
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({
+  //     error: 'missing name and/or number',
+  //   })
+  // }
 
   const persons = await Person.find({})
   const names = persons.map((person) => person.name.toLowerCase())
@@ -95,13 +107,40 @@ app.post('/api/persons', async (request, response) => {
   //   console.log('error saving to database')
   //   console.log(error)
   // })
-  const savedPerson = await person.save()
-  response.json(savedPerson)
+  try {
+    const savedPerson = await person.save()
+    response.json(savedPerson)
+  } catch (error) {
+    console.log(error.name)
+    next(error)
+  }
 
   // persons = [...persons, person]
   // console.log(body)
   // console.log(person)
   // response.json(person)
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+  // const person = {
+  //   name: body.name,
+  //   number: body.number,
+  // }
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedPerson) => {
+      console.log('tiger')
+      response.json(updatedPerson)
+    })
+    .catch((error) => {
+      console.log('testing')
+      next(error)
+    })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -110,7 +149,18 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
+  console.log('started')
 })
--- models/person.js
+++ models/person.js
@@ -12,17 +12,30 @@ mongoose
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-  })
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d*/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  },
+})
 
-  personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-  })
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-  module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema)
